1dc825aa8c1bc1dacd406974afd036790273d694
Emre Altunbilek <emrealtunbilek@gmail.com>
2019-01-25 00:59:20 +0300

    local olarak dosya olustuurldu içerik yazıldı ve okundu

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_storage_dersleri/dosya_islemleri.dart';
 import 'package:flutter_storage_dersleri/shared_pref_kullanimi.dart';
 
 void main() => runApp(MyApp());
@@ -12,7 +13,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: SharedPrefKullanimi(),
+      home: DosyaIslemleri(),
     );
   }
 }
