f0c7551fe3265c0bd2002fb543ac859e08b36ef0
Emre Altunbilek <emrealtunbilek@gmail.com>
2019-01-26 11:09:10 +0300

    sqlite giriş yapıldı ve model sınıfı olusturuldu

---
+++ lib/sqlflite_islemleri.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_storage_dersleri/model/ogrenci.dart';
+import 'package:sqflite/sqflite.dart';
+
+class SqfliteIslemleri extends StatefulWidget {
+  @override
+  _SqfliteIslemleriState createState() => _SqfliteIslemleriState();
+}
+
+class _SqfliteIslemleriState extends State<SqfliteIslemleri> {
+  @override
+  Widget build(BuildContext context) {
+
+    Ogrenci emre=Ogrenci.withID(10, "emre", true);
+    Map olusanMap = emre.toMap();
+    debugPrint(olusanMap['ad_soyad'].toString());
+
+    Ogrenci kopyaEmre = Ogrenci.fromMap(olusanMap);
+    debugPrint(kopyaEmre.toString());
+
+
+
+    return Scaffold(
+      appBar: AppBar(title: Text("SqfLite Kullanimi"),),
+      body: Center(child: Text("Boş"),),
+    );
+  }
+}
